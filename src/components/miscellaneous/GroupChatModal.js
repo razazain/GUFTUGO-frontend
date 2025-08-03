21c52b6586a5b58795cdf96f1ccd94b1063d45c6
razazain <razazain861@gmail.com>
2025-08-03 21:00:04 +0500

    debug 2

--- src/components/miscellaneous/GroupChatModal.js
+++ src/components/miscellaneous/GroupChatModal.js
@@ -9,7 +9,7 @@ const GroupChatModal = ({ children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [groupChatName, setGroupChatName] = useState();
   const [selectedUsers, setSelectedUsers] = useState([]);
-  const [, setSearch] = useState("");
+  const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const toast = useToast();
